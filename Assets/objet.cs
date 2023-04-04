ed856d88e5ec2af5d020ab77586a04c64d8573b8
LAPTOP-F4LUB3GV\antoine <antoine.peyronne@live.fr>
2023-04-04 14:36:45 +0200

    no message

--- Assets/objet.cs
+++ Assets/objet.cs
@@ -11,19 +11,42 @@ public class objet : MonoBehaviour
 
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+ 
            
     }
     void Update()
     {
+        //Quand on fait clique gauche
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
+            //Lancer un rayon dans la direction de la caméra
             Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
             RaycastHit hit;
+            //Action du rayon
             if (Physics.Raycast(ray, out hit))
             {
-                transform.position = hit.point;
+                //On prend le rigidbody de l'élément toucher par le rayon
+                rigidbody = hit.transform.GetComponent<Rigidbody>();
+                //Désactive la gravitée
+                rigidbody.useGravity = false;
+                rigidbody.isKinematic = true;
+                // Rapporcher le sac
+                hit.transform.position = transform.position + (transform.forward * 2) ;
+                hit.transform.parent = transform; 
+
+                
             }
+        }
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+                //Désactive la gravitée
+                rigidbody.useGravity = true;
+                rigidbody.isKinematic = false;
+            // Rapporcher le sac
+            rigidbody.transform.parent = null;
+
+
+            
         }
     }
 }
