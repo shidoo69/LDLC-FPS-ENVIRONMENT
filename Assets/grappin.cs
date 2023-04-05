29fa18752522aebebe7875ea6c9bf81a09dfc28f
LAPTOP-F4LUB3GV\antoine <antoine.peyronne@live.fr>
2023-04-05 17:56:47 +0200

    FIN GRQPPIN + PERSO

--- Assets/grappin.cs
+++ Assets/grappin.cs
@@ -7,12 +7,27 @@ public class grappin : MonoBehaviour
 {
     Vector3 destination;
     Vector3 position2depart;
-    public float temps;
-    float timer; 
+    public float vitesse; 
+    float temps;
+    float timer;
     // Start is called before the first frame update
-    void Start()
+    public LineRenderer laserLineRenderer;
+    public float laserWidth = 0.1f;
+
+    public float maxDistance;
+    void ShootLaserFromTargetPosition(Vector3 targetPosition, Vector3 direction, float length)
     {
-        
+        Ray ray = new Ray(targetPosition, direction);
+        RaycastHit raycastHit;
+        Vector3 endPosition = targetPosition + (length * direction);
+
+        if (Physics.Raycast(ray, out raycastHit, length))
+        {
+            endPosition = raycastHit.point;
+        }
+
+        laserLineRenderer.SetPosition(0, targetPosition);
+        laserLineRenderer.SetPosition(1, endPosition);
     }
 
     // Update is called once per frame
@@ -22,11 +37,16 @@ public class grappin : MonoBehaviour
         {
             Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit,maxDistance))
             {
                 destination = hit.point;
                 timer = 0;
                 position2depart = transform.position;
+                ShootLaserFromTargetPosition(Camera.main.transform.position+Camera.main.transform.right*-0.5f, (destination-position2depart).normalized, maxDistance);
+                laserLineRenderer.enabled = true;
+                float distance = Vector3.Distance(destination, position2depart);
+                temps = distance/vitesse;
+
             }
        
         }
@@ -36,15 +56,17 @@ public class grappin : MonoBehaviour
             timer = timer + Time.deltaTime;
             float ratio = timer / temps;
             transform.position = Vector3.Lerp(position2depart, destination, ratio);
-            
+           // Debug.Break();
             if (timer > temps)
             {
                 destination = Vector3.zero;
+                laserLineRenderer.enabled = false;
+
             }
-           
+
 
         }
-        
+
 
     }
 }
