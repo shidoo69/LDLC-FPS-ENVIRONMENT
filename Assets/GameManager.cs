2784db6f8896f7bc20e7e68d77ae3e2e267ec52f
Mateo <mateo.heurtel@gmail.com>
2023-04-05 17:40:00 +0200

    no message

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,24 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using System;
-using UnityEngine.SocialPlatforms.Impl;
 
 using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-    public TextMeshProUGUI scoreText;
+    public TMP_Text scoreText;
+    
     private int score;
     void Start()
     {
         UpdateScore(0);
+
         
     }
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Score: " + score + " / 5" ;
+
 
     }
     private void Awake()
